6cbde8443f7a8e374e587937a457746829674eb6
JeffCW <changcheewah96@gmail.com>
2021-01-08 23:35:04 +0800

    add bubble effect

--- client/src/components/MainPage/Main.js
+++ client/src/components/MainPage/Main.js
@@ -10,9 +10,11 @@ export default function Main() {
                 <Bubble size="20" top="-15" left="150" />
                 <Bubble size="40" top="-65" left="110" />
                 <div className="bubble-container">
-                    <Bubble size="20" top="0" left="0" />
-                    <Bubble size="20" top="0" left="0" />
-                    <Bubble size="20" top="0" left="0" />
+                    <Bubble size="5" top="0" left="0" />
+                    <Bubble size="5" top="0" left="0" />
+                    <Bubble size="5" top="0" left="0" />
+                    <Bubble size="5" top="0" left="0" />
+                    <Bubble size="5" top="0" left="0" />
                 </div>
                 <h1>CONNECT YOU & ME</h1>
             </div>

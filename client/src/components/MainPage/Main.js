01620207b7b793699150722bba3dc756538e3075
JeffCW <changcheewah96@gmail.com>
2021-01-11 23:10:53 +0800

    login ui

--- client/src/components/MainPage/Main.js
+++ client/src/components/MainPage/Main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Google from './Google'
 import './Main.css'
 import Bubble from './Bubble'
+import image from './logo.jpg'
 
 export default function Main() {
     return (
@@ -17,6 +18,17 @@ export default function Main() {
                     <Bubble size="5" top="0" left="0" />
                 </div>
                 <h1>CONNECT YOU & ME</h1>
+                <div className="flex align-top hori-between">
+                    <div style={{ width: '65%' }}>
+                        <img src={image} alt="logo image" style={{ maxWidth: '75%' }} />
+                    </div>
+                    <div className="sign-up-in-form">
+                        <div className="flex hori-center">
+                            <h2>Sign In</h2>
+                            <h2>Sign Up</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
